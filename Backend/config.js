2c210ee447e7d6fe89129845480f0e983409c234
Krish Patel <krishpatel@Krishs-MacBook-Pro.local>
2024-09-08 13:17:45 -0700

    Removed Unnessasry Files

--- Backend/config.js
+++
@@ -1,8 +0,0 @@
-
-MONGO_URL = 'mongodb+srv://krishpatel632:krish123@cluster0.bnp6rj9.mongodb.net/KrishCourseWebsite?retryWrites=true&w=majority'; 
-JWT_PASS = 'KrishCourseWebsite';     
-
-module.exports = {
-    MONGO_URL : MONGO_URL,
-    JWT_PASS : JWT_PASS  
-}
